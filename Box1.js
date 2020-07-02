0dfbfd5974ff07d2f73c6f2dd3593a0b447c2030
AJITESH3210 <jalluri3210@gmail.com>
2020-07-02 11:47:28 +0530

    Update Box1.js

--- Box1.js
+++ Box1.js
@@ -13,7 +13,7 @@ class Box1 extends BaseClass {
       push();
       this.Visiblity = this.Visiblity - 5;
       tint(255,this.Visiblity);
-      
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
       pop();
        }
     }
